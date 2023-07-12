7d8b5d14f522ccc2456ca2690a65ad8f0689d8a9
Grandic <d.bitkov@gmail.com>
2023-07-12 21:29:19 +0300

    Add new exceptions for Video class

--- src/video.py
+++ src/video.py
@@ -8,17 +8,24 @@ class Video:
     def __init__(self, video_id: str) -> None:
         self.video_id = video_id
         youtube = build('youtube', 'v3', developerKey=api_key)
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=self.video_id
-                                               ).execute()
-        self.video_title: str = video_response['items'][0]['snippet']['title']
-        self.url = f"https://www.youtube.com/watch/{video_response['items'][0]['id']}"
-        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
-        self.comment_count: int = video_response['items'][0]['statistics']['commentCount']
-
+        try:
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                id=self.video_id
+                                                ).execute()
+            self.title: str = video_response['items'][0]['snippet']['title']
+            self.url = f"https://www.youtube.com/watch/{video_response['items'][0]['id']}"
+            self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+            self.comment_count: int = video_response['items'][0]['statistics']['commentCount']
+        except IndexError:
+            print("Введен несуществующий id видео")
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
+            self.comment_count = None
     def __str__(self):
-        return self.video_title
+        return self.title
 
 
 class PLVideo(Video):
